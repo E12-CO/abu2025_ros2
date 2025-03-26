48d90e18e67b62a5c9786ac90be7f0346374eb0e
TiNredmc <thipok17@gmail.com>
2025-03-26 17:09:06 +0700

    added iRob man3uver to sim launch file plus params
    Signed-off-by: TiNredmc <thipok17@gmail.com>

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -51,11 +51,37 @@ def generate_launch_description():
                         namespace='r2',
                         output='screen')
 
+    # Run the iRob_maneuv3r controller
+    irob_maneuv3r_r1_instant = Node(
+        package='irob_maneuv3r',
+        executable='iRob_maneuv3r',
+        #name='irob_maneu3r_r1',
+        namespace = 'r1',
+        output='screen',
+        remappings=[
+            ('cmd_vel_irob_auto', 'cmd_vel'),
+            ],
+        parameters=[os.path.join(get_package_share_directory(package_name), 'params_global', 'irob_maneuv3r_sim_r1.yaml')]
+    )
+    
+    irob_maneuv3r_r2_instant = Node(
+        package='irob_maneuv3r',
+        executable='iRob_maneuv3r',
+        #name='irob_maneu3r_r2',
+        namespace = 'r2',
+        output='screen',
+        remappings=[
+            ('cmd_vel_irob_auto', 'cmd_vel'),
+            ],
+        parameters=[os.path.join(get_package_share_directory(package_name), 'params_global', 'irob_maneuv3r_sim_r2.yaml')]
+    )
 
     # Launch them all!
     return LaunchDescription([
         rsp,
         ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so',world_path], output='screen'),
         spawn_r1,
-        spawn_r2
+        spawn_r2,
+        irob_maneuv3r_r1_instant,
+        irob_maneuv3r_r2_instant
     ])
